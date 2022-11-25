e42d0ea65e282a3e7eb47c04c91d9b434fd409c4
Dharmar <dharmaraws@gmail.com>
2022-11-25 09:45:55 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+      <h1 className="text-blue-700 text-3xl font-bold">
+        Blockchain Development with JavasxriptMastery
+      </h1>
+  );
+}
+
+export default Home;
